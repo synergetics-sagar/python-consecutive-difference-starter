92d9a84f5b11eaad8d67ef490a96b6f786bd49e0
Sagar Mahajan <sagarmahajan31997@gmail.com>
2026-02-05 14:43:37 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,7 @@
+def has_consecutive_diff_one(arr):
+    """
+    Returns True if the difference between all consecutive elements
+    is exactly 1 or -1, otherwise False.
+    """
+    # TODO: Write your logic here
+    pass
